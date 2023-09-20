983abaed4a05fd43799b291762cac513829310f5
Elie AMAR <eamar@made-f0Br8s12.clusters.42paris.fr>
2023-09-20 20:31:15 +0200

    arthur & elie - ajout de la 3D

--- requirements/react/app/src/Home.tsx
+++ requirements/react/app/src/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Menu from './Menu';
 
+import ModelViewer from './components/ModelViewer/ModelViewer';
 
 function home(): JSX.Element {
 	return (
@@ -11,6 +12,7 @@ function home(): JSX.Element {
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean congue scelerisque neque tincidunt dapibus. Vestibulum porta metus ac eros dapibus ornare. Mauris ex nulla, posuere sit amet sem a, egestas tempor justo. Sed convallis tempus ligula, ac molestie ex aliquam sit amet. Phasellus placerat est pellentesque, ornare nunc eu, sagittis mauris. In vestibulum lectus sit amet tortor dapibus ornare. Aliquam justo nunc, pharetra et urna eu, vehicula vehicula diam. Mauris ac elit in purus commodo pellentesque.
 				Pellentesque accumsan laoreet diam fermentum elementum. Suspendisse potenti. Maecenas consequat pharetra enim, vel imperdiet sem bibendum blandit. Nulla facilisi. Vestibulum tempus vulputate ultricies. Donec at est a erat feugiat molestie eget nec quam. Mauris id dolor metus. Curabitur quis suscipit sem, quis tempus urna. Morbi blandit malesuada mi vel placerat. Phasellus ultrices ipsum nec ultrices scelerisque.
 			</div>
+			<ModelViewer/>
 		</div>
 	);
 }
